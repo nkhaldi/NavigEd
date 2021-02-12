e1ecc27c8e7a635ccc4f70a6e2b8e1467b91502b
nkhaldi <mnk80898@gmail.com>
2021-02-12 14:19:39 +0300

    modified mesage hanler

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -17,18 +17,27 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
     greeting = f"<b>Привет {message.from_user.first_name}!</b>\n"
-    final_message = greeting + "Что хочешь изучить?"
-    send_mess = final_message
-    bot.send_message(message.chat.id, send_mess, parse_mode='html')
+    fin_msg = greeting + "Что хочешь изучить?"
+    bot.send_message(message.chat.id, fin_msg, parse_mode='html')
 
 
 @bot.message_handler(content_types=['text'])
 def mess(message):
     get_message = message.text.strip().lower()
-#    subject = get_subject(get_message)
-    subject = get_message
-    final_message = f"Ты выбрал <b>{subject}</b>."
-    bot.send_message(message.chat.id, final_message, parse_mode='html')
+    subject = get_subject(get_message)
+    fin_msg = f"Ты выбрал <b>{subject}</b>."
+    bot.send_message(message.chat.id, fin_msg, parse_mode='html')
+
+    fin_msg = "<b>Как ты предпочитаешь учиться?</b>\n"
+    fin_msg += "- Статьи\n"
+    fin_msg += "- Книги\n"
+    fin_msg += "- Интерактивные курсы\n"
+    fin_msg += "- Видео\n"
+    bot.send_message(message.chat.id, fin_msg, parse_mode='html')
+
+    method = get_message = message.text.strip().lower()
+    fin_msg = f"Ты выбрал <b>{method}</b>."
+    bot.send_message(message.chat.id, fin_msg, parse_mode='html')
 
 
 bot.polling(none_stop=True)
