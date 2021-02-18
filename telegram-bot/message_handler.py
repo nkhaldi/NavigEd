4861213eeb65d1399939425afa8d05c905d71a5a
nkhaldi <mnk80898@gmail.com>
2021-02-19 00:53:17 +0300

    added message_handler.py

---
+++ telegram-bot/message_handler.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def is_greeting(msg):
+    if msg == "hello" or    \
+            msg == "hi" or  \
+            msg == "привет":
+        return True
+    else:
+        return False
