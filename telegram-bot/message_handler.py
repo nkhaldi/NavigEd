3c9ff6a6280f077fbdbb61be3170a7ff121b5dc8
nkhaldi <mnk80898@gmail.com>
2021-02-20 14:18:06 +0300

    replaced get_subject from navigator to message handler

--- telegram-bot/message_handler.py
+++ telegram-bot/message_handler.py
@@ -8,3 +8,19 @@ def is_greeting(msg):
         return True
     else:
         return False
+
+
+def get_subject(msg):
+    for subj in subjects:
+        if msg in subjects[subj]:
+            return subj
+    return False
+
+
+subjects = {
+        'ai':   ['ai', 'ии', 'искуственный интеллект'],
+        'ds':   ['ds', 'data science'],
+        'eng':  ['eng', 'english', 'английский'],
+        'math': ['math', 'матан', 'математика'],
+        'prog': ['prog', 'coding', 'programming', 'программирование']
+}
--- telegram-bot/navigator.py
+++ telegram-bot/navigator.py
@@ -12,38 +12,3 @@ def navigate(subject, method):
         for row in reader:
             res += f"{row}"
     return res
-
-
-subj_codes = {
-    # ai
-    'ai': 'ai',
-    'ии': 'ai',
-    'искуственный интеллект': 'ai',
-
-    # ds
-    'ds': 'ds',
-    'data science': 'ds',
-
-    # eng
-    'eng': 'eng',
-    'english': 'eng',
-    'английский': 'eng',
-
-    # math
-    'math': 'math',
-    'матан': 'math',
-    'математика': 'math',
-
-    # prog
-    'prog': 'prog',
-    'coding': 'prog',
-    'programming': 'prog',
-    'программирование': 'prog'
-}
-
-
-def get_subject(msg):
-    if msg in subj_codes:
-        return subj_codes[msg]
-    else:
-        return False
