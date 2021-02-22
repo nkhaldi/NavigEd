e77af6845f84147091cb20fae15d654395462928
nkhaldi <mnk80898@gmail.com>
2021-02-22 21:22:04 +0300

    added methods

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -15,6 +15,11 @@ token_file = open('/home/narek/.pass/.naviged.token')
 token = token_file.read().rstrip('\n')
 bot = telebot.TeleBot(token)
 
+ms = {
+        's': '',
+        'm': ''
+}
+
 
 @bot.message_handler(commands=['start'], content_types=['text'])
 def start(message):
@@ -31,16 +36,24 @@ def mess(message):
         return start(message)
 
     subject = get_subject(msg_in)
-    if not subject:
+    method = get_method(msg_in)
+    method = 'a'
+
+    if not subject and not method:
         msg_out = "Я пока этого не знаю."
         bot.send_message(message.chat.id, msg_out, parse_mode='html')
         return
+    elif subject:
+        ms['s'] = subject
+        msg_out = f"<b>Как ты хочешь изучать {msg_in}?</b>\n"
+        msg_out += "- Статьи\n"
+        msg_out += "- Книги\n"
+        msg_out += "- Интерактивные курсы\n"
+        msg_out += "- Видео\n"
+    else:
+        ms['m'] = method
+        msg_out = navigate(ms['s'], ms['m'])
 
-    msg_out = f"<b>Как ты хочешь изучить {msg_in}?</b>\n"
-    msg_out += "- Статьи\n"
-    msg_out += "- Книги\n"
-    msg_out += "- Интерактивные курсы\n"
-    msg_out += "- Видео\n"
     bot.send_message(message.chat.id, msg_out, parse_mode='html')
 
 
