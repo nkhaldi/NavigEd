4412642fefab845e9e31efed73fa1b4c13b8560d
nkhaldi <mnk80898@gmail.com>
2021-02-22 21:21:49 +0300

    added get_method

--- telegram-bot/message_handler.py
+++ telegram-bot/message_handler.py
@@ -17,10 +17,24 @@ def get_subject(msg):
     return False
 
 
+def get_method(msg):
+    for meth in methods:
+        if msg in methods[meth]:
+            return meth
+    return False
+
+
 subjects = {
         'ai':   ['ai', 'ии', 'искуственный интеллект'],
         'ds':   ['ds', 'data science'],
         'eng':  ['eng', 'english', 'английский'],
         'math': ['math', 'матан', 'математика'],
         'prog': ['prog', 'coding', 'programming', 'программирование']
+}
+
+methods = {
+        'a': ['a', 'articles', 'статьи'],
+        'b': ['b', 'books', 'книги'],
+        'c': ['c', 'courses', 'курсы', 'интерактивные курсы', 'онлайн-курсы'],
+        'v': ['v', 'videos', 'видео', 'видеокурсы']
 }
