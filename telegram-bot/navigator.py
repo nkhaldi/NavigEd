1c41128cdb1ab044e4b122a03af042c86ad7e266
nkhaldi <mnk80898@gmail.com>
2021-02-25 22:04:18 +0300

    added parsers

--- telegram-bot/navigator.py
+++ telegram-bot/navigator.py
@@ -4,11 +4,47 @@
 from csv import reader as csvReader
 
 
+def parse_article(row):
+    name, url = row
+    res = f"{name} - {url}\n"
+    return res
+
+
+def parse_book(row):
+    name, author, year = row
+    res = f"{name} - {author} {[year]}\n"
+    return res
+
+
+def parse_course(row):
+    name, url = row
+    res = f"{name} - {url}\n"
+    return res
+
+
+def parse_video(row):
+    name, url = row
+    res = f"{name} - {url}\n"
+    return res
+
+
+def parse_file(reader, method):
+    res = ""
+    for row in reader:
+        if method == 'a':
+            res += parse_article(row)
+        elif method == 'b':
+            res += parse_book(row)
+        elif method == 'c':
+            res += parse_course(row)
+        elif method == 'v':
+            res += parse_video(row)
+    return res
+ 
+
 def navigate(subject, method):
     fname = '../db/' + str(subject) + '/' + str(method) + '.csv'
     with open(fname, 'r') as fd:
         reader = csvReader(fd)
-        res = ""
-        for row in reader:
-            res += f"{row}\n"
+        res = parse_file(reader, method)
     return res
