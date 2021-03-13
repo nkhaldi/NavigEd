7b9429843dc6fd0371913893db8e4dedf80de38e
nkhaldi <mnk80898@gmail.com>
2021-03-13 13:18:19 +0300

    changed keyboard import

--- src/boards.py
+++ src/boards.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 
-import telebot
+from telebot.types import ReplyKeyboardMarkup as keyboard
 
 
 # Subjects menu
 def get_subject_board():
-    subject_board = telebot.types.ReplyKeyboardMarkup(True, True)
+    subject_board = keyboard(True, True)
     subject_board.row('Математика', 'Программирование')
     subject_board.row('Языки', 'Прочее')
     subject_board.row('Поддержать')
@@ -15,7 +15,7 @@ def get_subject_board():
 
 # Methods menu
 def get_method_board():
-    method_board = telebot.types.ReplyKeyboardMarkup(True, True)
+    method_board = keyboard(True, True)
     method_board.row('Статьи', 'Книги')
     method_board.row('Онлайн-курсы', 'Видео')
     method_board.row('Назад', 'Поддержать')
@@ -23,6 +23,6 @@ def get_method_board():
 
 
 def get_code_board(codes):
-    code_board = telebot.types.ReplyKeyboardMarkup(True, True)
+    code_board = keyboard(True, True)
     code_board.row('Основы', 'Поддержать')
     return code_board
